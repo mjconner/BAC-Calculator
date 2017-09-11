23d48015d7dec177cbef7f2a43e55f5ce92cadfd
Madeline Conner <maddyjconner@gmail.com>
2017-09-11 13:33:12 -0700

    Added time functionality to check for amount of time elapsed and adjust BAC accordingly

--- calculations.js
+++ calculations.js
@@ -1,4 +1,8 @@
-function userClick() {
+// Given a gender, weight, and number of standard drinks consumed over a period of time, 
+// this function will calculate your approximate BAC level based on the Widmark Formula
+// (You can read more about this here: https://en.wikipedia.org/wiki/Blood_alcohol_content)
+
+function calculateBAC() {
     "use strict";
     var gender = document.getElementById("gender").value;
     var genderConst;
@@ -13,5 +17,6 @@ function userClick() {
     var weight = document.getElementById("weight").value * 454;
     var gramsOfAlcohol = document.getElementById("drink_count").value * 14;
     var bac = (gramsOfAlcohol / (weight * genderConst)) * 100;
+    bac = bac - (document.getElementById("time_elapsed").value * 0.015);
     alert("Your BAC is: " + bac);
 }
